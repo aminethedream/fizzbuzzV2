6c15c38fe3ffa7112767f5464cb9dddba5b57805
Amine TheDream <aminethedream@Amines-MacBook-Air.local>
2013-11-18 14:10:09 -0500

    add test

---
+++ rspec_fizzbuzzV2.rb
@@ -0,0 +1,11 @@
+require "./fizzbuzzV2"
+
+describe "#fizzbuzzV2"
+	it description "test whether number 15 comes out to fizzbuzz" do 
+		expect (fizzbuzz(15)).to do eq ("fizzbuzz")
+	end 
+	it description "test whether number 7 comes out to "do 
+		expect (fizzbuzz(7)).to do eq (7)
+		end 
+		end
+	end
