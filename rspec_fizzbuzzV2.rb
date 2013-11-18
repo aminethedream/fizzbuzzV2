9f61cafd08d4161edd3dc6a6a13a1063770fb6f2
Amine TheDream <aminethedream@Amines-MacBook-Air.local>
2013-11-18 14:13:32 -0500

    improve test

--- rspec_fizzbuzzV2.rb
+++ rspec_fizzbuzzV2.rb
@@ -2,10 +2,17 @@ require "./fizzbuzzV2"
 
 describe "#fizzbuzzV2"
 	it description "test whether number 15 comes out to fizzbuzz" do 
-		expect (fizzbuzz(15)).to do eq ("fizzbuzz")
+		expect (fizzbuzz(15)).to do eq("fizzbuzz")
 	end 
 	it description "test whether number 7 comes out to "do 
-		expect (fizzbuzz(7)).to do eq (7)
-		end 
+		expect (fizzbuzz(7)).to do eq(7)
+	end 
+	it description "test whether number 10 comes out to buzz" do 
+		expect (fizzbuzz(10)).to do eq("buzz")
+	end 
+	it description "test whether number 6 comes out to fizz" do 
+		expect (fizzbuzz(6)).to do eq("fizz")
+	end 
+
 		end
 	end
